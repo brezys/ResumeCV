e59be84b83ae236f4f4d6b287bc04b8fe53977f2
Nick Brezinski <108705036+brezys@users.noreply.github.com>
2025-07-24 07:04:40 -0400

    Updated resume pdf

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -220,7 +220,7 @@ const Contact = () => {
                   </p>
                   
                   <motion.a
-                    href="/resumenick.pdf"
+                    href="/resumenicknew.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                     whileHover={{ scale: 1.02 }}
